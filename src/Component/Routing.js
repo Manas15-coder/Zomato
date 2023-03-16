6ee1135d66b7844c08d529b4c5aebb8768465a44
Manas15-coder <manas15072002@gmail.com>
2023-03-16 21:40:08 +0530

    first commit

---
+++ src/Component/Routing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home/Home';
+import Listing from './Listing/listing';
+import Details from './details/details';
+import ViewOrder from './orders/viewOrder';
+import PlaceOrder  from './orders/placeOrder';
+
+const Routing = () => {
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/listing/:mealId" component={Listing}/>
+            <Route path="/details" component={Details}/>
+            <Route path="/placeOrder/:restName" component={PlaceOrder}/>
+            <Route path="/viewBooking" component={ViewOrder}/>
+            <Footer/>
+        </BrowserRouter>
+    )
+}
+
+
+export default Routing
